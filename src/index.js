014ec3cde9abc85162c622176c58f077742ce469
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-02 23:48:00 -0300

    Fix bug on main middleware

--- src/index.js
+++ src/index.js
@@ -34,22 +34,32 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  const { user } = request;
+  const { username } = request.headers;
   const { id } = request.params;
 
-  if (!validate(id)) {
-    return response.status(400).json({ error: 'Invalid todo ID' });
-  }
+  const user = users.find((user) => user.username === username);
 
-  const todo = user?.todos?.find((todo) => todo.id === id);
+  if (!user) return response.status(404).json({ 
+    error: 'User not found' 
+  });
+  
+  const idIsUuid = validate(id);
 
-  if (!todo) {
-    return response.status(404).json({ error: 'Todo not found' });
-  }
+  if (!idIsUuid) 
+    return response.status(400).json({
+       error: 'Id must be a valid uuid' 
+    });
 
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) return response.status(404).json({ 
+    error: 'Todo not found' 
+  });
+
+  request.user = user;
   request.todo = todo;
 
-  return next();
+  next();
 }
 
 function findUserById(request, response, next) {
